5a165b4fea72a3e81d7befa37404a1f673fffd54
neal <neal.dam.1008@gmail.com>
2020-11-08 16:04:52 +0700

    create project

---
+++ src/main/java/com/nealdam/tntttool/churchpowerpointgenerator/ChurchPowerpointGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.nealdam.tntttool.churchpowerpointgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchPowerpointGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchPowerpointGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nealdam/tntttool/churchpowerpointgenerator/ChurchPowerpointGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nealdam.tntttool.churchpowerpointgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchPowerpointGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
